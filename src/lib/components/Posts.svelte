74f86f962f7c960d1362609a82aa06605d9b5006
teauxfu <teauxfu@icloud.com>
2026-01-12 21:35:20 -0600

    simplify date handling

--- src/lib/components/Posts.svelte
+++ src/lib/components/Posts.svelte
@@ -6,6 +6,7 @@
 	};
 
 	let { data } = $props();
+	// NOTE on weird date parsing https://stackoverflow.com/a/31732581
 </script>
 
 <nav class="mx-auto flex max-w-xl grow p-2">
@@ -13,13 +14,13 @@
 		{#each data.posts as post}
 			<li class="flex flex-col gap-1 rounded-md border border-dsa-red3 p-2 shadow-sm">
 				<a href="/blog/{post.slug}" class="text-3xl underline decoration-dsa-red">{post.title}</a>
-				<div class="flex max-w-3/4 flex-row justify-between">
+				<div class="flex flex-row justify-between">
 					{#if post.title}
 						<span>{post.author}</span>
 					{/if}
 					{#if post.date}
 						<em>
-							<time>{new Date(post.date).toLocaleDateString("en-us", options)}</time>
+							<time>{new Date(post.date.replace(/-/g, '\/').replace(/T.+/, '')).toLocaleDateString("en-us", options)}</time>
 						</em>
 					{/if}
 				</div>
